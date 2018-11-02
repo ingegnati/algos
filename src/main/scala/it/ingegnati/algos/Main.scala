057e5d20ba5d3420b18c56ccf460f5ba9b7dc852
Alfredo <alfredotorre@gmail.com>
2018-11-02 19:43:28 +0000

    Fix licensing

--- src/main/scala/it/ingegnati/algos/Main.scala
+++ src/main/scala/it/ingegnati/algos/Main.scala
@@ -1,5 +1,13 @@
 package it.ingegnati.algos
 
+// TODO: implement `show w`
 object Main extends App {
+    val license = """
+Algos  Copyright (C) 2018  ingegnati.it
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w`.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details.
+    """
+    println(license)
     println("Hello from Scala Algos!")
 }
